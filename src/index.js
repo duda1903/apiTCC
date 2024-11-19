c92bd40c7f42026bcff7b1ef8c74658eef25f973
eduarda <eduarda-tavares1@educar.rs.gov.br>
2024-11-19 16:37:57 -0300

    mudançaass

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ const routes = require('./routes');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+// Configuração do CORS - permitindo apenas origens específicas
+const corsOptions = {
+  origin: 'http://localhost:*', // Aqui você pode adicionar o domínio ou lista de domínios que deseja permitir
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+};
+
+app.use(cors(corsOptions));  // Aplique o CORS em todas as rotas
+
 app.use(bodyParser.json());
 app.use('/api', routes);
 
