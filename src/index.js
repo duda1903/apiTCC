3314667704e1a7863c906dcfb3a66a4405e1a79c
eduarda <eduarda-tavares1@educar.rs.gov.br>
2024-11-19 20:55:29 -0300

    adicionandmo cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
+const cors = require('cors');  // Importe o pacote cors
 const routes = require('./routes');
 
 const app = express();
 const port = 3000;
 
-app.use(cors());
+// Permitir requisições de qualquer origem (qualquer IP)
+app.use(cors());  
+
 app.use(bodyParser.json());
 app.use('/api', routes);
 
