3261b671ed700d792e9ddd3d344cd07bc3a56426
eduarda <eduarda-tavares1@educar.rs.gov.br>
2024-11-19 19:41:16 -0300

    subindo api para o fly.io

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 const port = 3000;
 
-// Configuração do CORS - permitindo apenas origens específicas
-const corsOptions = {
-  origin: 'http://localhost:*', // Aqui você pode adicionar o domínio ou lista de domínios que deseja permitir
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
-};
-
-app.use(cors(corsOptions));  // Aplique o CORS em todas as rotas
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', routes);
 
